bb8ab8048b6b7f3e6ccff59f268e98a0997768ea
Your name <a.m.verdine@gmail.com>
2020-04-19 17:59:02 -0400

    Install dependencies/Setup .env

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+// this reads in all of our environment variables from .env
+require("dotenv").config();
 const server = require("./api/server.js");
+const welcome = require("./welcome/welcome-router");
+
+server.use("/", welcome);
 
 const port = 5000;
 server.listen(port, () => {
